a306a5d2f0a9ed2d34260e9f364925706e608748
lmorocoramos <lmorocoramos@gmail.com>
2025-08-24 23:08:30 -0500

    build: add common exceptions.

---
+++ src/main/java/com/crediya/common/exc/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.crediya.common.exc;
+
+public class BadRequestException extends BaseException {
+  public BadRequestException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/crediya/common/exc/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.crediya.common.exc;
+
+public class NotFoundException extends BaseException{
+  public NotFoundException(String message) {
+    super(message);
+  }
+}
