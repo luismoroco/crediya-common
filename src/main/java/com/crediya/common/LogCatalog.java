2846ffc4dcd66bcbace4904d8a26ca448e261b09
lmorocoramos <lmorocoramos@gmail.com>
2025-08-30 14:10:22 -0500

    refactor: make utils generic.

--- src/main/java/com/crediya/common/LogCatalog.java
+++ src/main/java/com/crediya/common/LogCatalog.java
@@ -3,83 +3,83 @@ package com.crediya.common;
 public enum LogCatalog {
   ENTITY_NOT_FOUND {
     @Override
-    public String get(String entityName) {
-      return String.format("%s not found", entityName);
+    public String of(Object identifier) {
+      return String.format("%s not found", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("%s %s not found", value, entityName);
+    public String of(Object identifier, Object value) {
+      return String.format("%s %s not found", value, identifier);
     }
   },
   INVALID_PARAMETER {
     @Override
-    public String get(String entityName) {
-      return String.format("Invalid parameter: %s", entityName);
+    public String of(Object identifier) {
+      return String.format("Invalid parameter: %s", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("%s %s invalid parameter", value, entityName);
+    public String of(Object identifier, Object value) {
+      return String.format("%s %s invalid parameter", value, identifier);
     }
   },
   ENTITY_ALREADY_EXISTS {
     @Override
-    public String get(String entityName) {
-      return String.format("%s already exists", entityName);
+    public String of(Object identifier) {
+      return String.format("%s already exists", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("%s %s already exists", value, entityName);
+    public String of(Object identifier, Object value) {
+      return String.format("%s %s already exists", value, identifier);
     }
   },
   BEGIN_PROCESSING {
     @Override
-    public String get(String entityName) {
-      return String.format("Begin Processing: %s", entityName);
+    public String of(Object identifier) {
+      return String.format("Begin Processing: %s", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("Begin Processing: %s with %s", entityName, value);
+    public String of(Object identifier, Object value) {
+      return String.format("Begin Processing: %s with %s", identifier, value);
     }
   },
   ERROR_PROCESSING {
     @Override
-    public String get(String entityName) {
-      return String.format("Error Processing: %s", entityName);
+    public String of(Object identifier) {
+      return String.format("Error Processing: %s", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("Error Processing: %s with %s", entityName, value);
+    public String of(Object identifier, Object value) {
+      return String.format("Error Processing: %s with %s", identifier, value);
     }
   },
   ENTITY_FOUND {
     @Override
-    public String get(String entityName) {
-      return String.format("%s found", entityName);
+    public String of(Object identifier) {
+      return String.format("%s found", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("%s %s found", entityName, value);
+    public String of(Object identifier, Object value) {
+      return String.format("%s %s found", identifier, value);
     }
   },
   SUCCESSFUL_PROCESSING {
     @Override
-    public String get(String entityName) {
-      return String.format("Successful Processing: %s", entityName);
+    public String of(Object identifier) {
+      return String.format("Successful Processing: %s", identifier);
     }
 
     @Override
-    public String get(String entityName, Object value) {
-      return String.format("Successful Processing: %s with %s", entityName, value);
+    public String of(Object identifier, Object value) {
+      return String.format("Successful Processing: %s with %s", identifier, value);
     }
   };
 
-  public abstract String get(String entityName);
+  public abstract String of(Object identifier);
 
-  public abstract String get(String entityName, Object value);
+  public abstract String of(Object identifier, Object value);
 }
--- src/main/java/com/crediya/common/validation/ValidatorUtils.java
+++ src/main/java/com/crediya/common/validation/ValidatorUtils.java
@@ -1,7 +1,7 @@
 package com.crediya.common.validation;
 
 import com.crediya.common.EmailUtils;
-import com.crediya.common.LogCatalog;
+import static com.crediya.common.LogCatalog.*;
 import com.crediya.common.PhoneUtils;
 import com.crediya.common.exc.ValidationException;
 
@@ -15,66 +15,51 @@ public class ValidatorUtils {
   private ValidatorUtils() {
   }
 
-  public static Mono<Void> string(String entityName, String value) {
-    if (Objects.isNull(value) || value.isBlank()) {
-      return Mono.error(new ValidationException(LogCatalog.INVALID_PARAMETER.get(entityName, value)));
-    }
-
-    return Mono.empty();
+  public static Mono<Void> string(Object identifier, String value) {
+    return (Objects.isNull(value) || value.isBlank())
+      ? Mono.error(new ValidationException(INVALID_PARAMETER.of(identifier, value)))
+      : Mono.empty();
   }
 
-  public static Mono<Void> phone(String entityName, String value) {
-    if (!PhoneUtils.isValid(value)) {
-      return Mono.error(new ValidationException(LogCatalog.INVALID_PARAMETER.get(entityName, value)));
-    }
-
-    return Mono.empty();
+  public static Mono<Void> phone(Object identifier, String value) {
+    return (!PhoneUtils.isValid(value))
+      ? Mono.error(new ValidationException(INVALID_PARAMETER.of(identifier, value)))
+      : Mono.empty();
   }
 
-  public static Mono<Void> email(String entityName, String value) {
-    if (!EmailUtils.isValid(value)) {
-      return Mono.error(new ValidationException(LogCatalog.INVALID_PARAMETER.get(entityName, value)));
-    }
-
-    return Mono.empty();
+  public static Mono<Void> email(Object identifier, String value) {
+    return (!EmailUtils.isValid(value))
+      ? Mono.error(new ValidationException(INVALID_PARAMETER.of(identifier, value)))
+      : Mono.empty();
   }
 
-  public static Mono<Void> localDate(String entityName, String value) {
-    return string(entityName, value)
+  public static Mono<Void> localDate(Object identifier, String value) {
+    return string(identifier, value)
       .then(Mono.defer(() -> {
         try {
           LocalDate.parse(value);
           return Mono.empty();
         } catch (DateTimeParseException e) {
-          return Mono.error(new ValidationException(
-            LogCatalog.INVALID_PARAMETER.get(entityName, value)
-          ));
+          return Mono.error(new ValidationException(INVALID_PARAMETER.of(identifier, value)));
         }
       }));
   }
 
-  public static Mono<Void> nonNull(String entityName, Object value) {
-    if (Objects.isNull(value)) {
-      return Mono.error(new ValidationException(
-        LogCatalog.INVALID_PARAMETER.get(entityName, null)
-      ));
-    }
-    return Mono.empty();
+  public static Mono<Void> nonNull(Object identifier, Object value) {
+    return (Objects.isNull(value))
+      ? Mono.error(new ValidationException(INVALID_PARAMETER.of(identifier, null)))
+      : Mono.empty();
   }
 
   public static <T extends Comparable<T>> Mono<Void> numberBetween(
-    String entityName,
+    Object identifier,
     T value,
     T start,
     T end
   ) {
-    return nonNull(entityName, value)
-      .then(Mono.defer(() -> {
-        if (value.compareTo(start) < 0 || value.compareTo(end) > 0) {
-          return Mono.error(new ValidationException(LogCatalog.INVALID_PARAMETER.get(entityName, value)));
-        }
-
-        return Mono.empty();
-      }));
+    return nonNull(identifier, value)
+      .then(Mono.defer(() -> (value.compareTo(start) < 0 || value.compareTo(end) > 0)
+        ? Mono.error(new ValidationException(INVALID_PARAMETER.of(identifier, value)))
+        : Mono.empty()));
   }
 }
